a930b714c10316a8b3c9638492546d1007f64934
Yann Hourdel <yann.hourdel@siit.io>
2026-03-15 00:11:23 +0100

    init r-b bootstrap

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -39,7 +39,7 @@ variable "github_repo" {
 # Terraform state backend
 # ---------------------------------------------------------------------------
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "${var.project_name}-tf-state"
+  bucket = "r-b-${var.project_name}-tf-state"
 
   lifecycle {
     prevent_destroy = true
@@ -73,7 +73,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_state_lock" {
-  name         = "${var.project_name}-tf-lock"
+  name         = "r-b-${var.project_name}-tf-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
